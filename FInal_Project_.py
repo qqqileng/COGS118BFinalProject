dfdf144b9925c584547ad4af91b2e03126e4d28b
qqqileng <qleng@ucsd.edu>
2018-12-10 23:28:58 -0800

    Add files via upload

--- FInal_Project_.py
+++ FInal_Project_.py
@@ -14,9 +14,10 @@ from sklearn.preprocessing import normalize
 #import tensorflow as tf
 from sklearn.mixture import GaussianMixture
 from sklearn.preprocessing import StandardScaler
-
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 #mnist = input_data.read_data_sets('./hmnist_28_28_RGB.csv', one_hot = True)
-medium_colored_data = pd.read_csv('./hmnist_28_28_RGB.csv')
+medium_colored_data = pd.read_csv('./hmnist_64_64_L.csv')
 
 medium_colored_data = medium_colored_data.values
 np.random.shuffle(medium_colored_data)
@@ -26,14 +27,16 @@ rownum = len(medium_colored_data)
 col = len(medium_colored_data[0])
 X = medium_colored_data[:, 0:col-1]
 Y = medium_colored_data[:, col-1]
-
+pca = PCA(n_components=20)
+pca.fit(X)
 model = GaussianMixture(n_components=8,
                         covariance_type="full",
                         n_init=10,
                         random_state=0)
 
 cluster = model.fit_predict(X)
-#medium_colored_data["cluster"] = cluster
+
+
 
 
     
